4b79aca2dfad9e6cf5a02afff380d2dea070300b
João Vitor <joaovitor20022004@gmail.com>
2026-03-07 02:50:03 -0300

    fix: ajuste de gitignore

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jv.jjreviews',
+  appName: 'JJ Reviews',
+  webDir: 'dist',
+};
+
+export default config;
