c04bca85c23e5e06da8c372c8e3111ad6617c58e
João Vitor <joaovitor20022004@gmail.com>
2026-03-22 19:29:06 -0300

    fix: verificação de erros adicionada

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,21 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@/features/auth";
 import "./styles/variables.css";
 import "./styles/global.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f6d31cb9913cd5a719b4d698826b34c8@o4511090247335936.ingest.de.sentry.io/4511090325520464", 
+  
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 0.1, 
+
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
